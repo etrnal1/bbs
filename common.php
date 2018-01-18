270487f5e1dfb079a29123be10eeb522b7396224
etrnal1 <etrnal1@users.noreply.github.com>
2018-01-19 00:46:43 +0800

    添加了 session_start
    避免每次连接数据库

--- common.php
+++ common.php
@@ -5,6 +5,7 @@
  * 数据库函数文件
  * 传递的参数一定要写的正确
  */
+session_start();
 define('WEB_SITE', str_replace('\\', '/', __DIR__) . '/');
 $database = include WEB_SITE . 'config/config.php';
 include WEB_SITE . 'function/base_func.php';
@@ -13,7 +14,7 @@ include WEB_SITE . 'function/mysql_func.php';
 //echo WEB_SITE;
 //var_dump($database);
 //exit();
-var_dump($database);
+//var_dump($database);
 $cd = mysqli_connect('127.0.0.1', 'root', '');
 //var_dump($cd);
 
