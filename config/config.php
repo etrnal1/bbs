7f03ddc65cecc32d9e1e91896ffae418851f45d4
etrnal1 <etrnal1@users.noreply.github.com>
2018-01-18 19:25:21 +0800

    添加了数据库的文件的配置

--- config/config.php
+++ config/config.php
@@ -2,6 +2,16 @@
 /**
  * 定制一些数据库的基本文件
  */
+return [
+   'DB_HOST' => 'localhost',
+
+   'DB_USER' =>'root',
+   'DB_PWD' => '',
+   'DB_NAME'=>'root',
+   'DB_CHARSET' =>'UTF8',
+   'DB_PREFIX' =>'bs_',
+ 
+]
 define(DB_HOST, '120.0.0.1');
 define(DB_NAME, 'bs');
 define(DB_USER, root);
