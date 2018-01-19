bab0ff26d18afdfe43d4fdd61af97f522947da3f
etrnal1 <etrnal1@users.noreply.github.com>
2018-01-19 11:11:36 +0800

    能够实现发表文章需要美化

--- fabiao.php
+++ fabiao.php
@@ -5,22 +5,17 @@
 	<title>发表文章</title>
 </head>
 <body>
-  <form action="request.php" method="post">
+  <form action="article.php" method="post">
   		<table width="800" border="1" >
   			标题 <input type="text" name="title" size="100"><br>
 
   			文章内容
-  			 <textarea name="conteny" cols="100" rows="20">
+  			 <textarea name="content" cols="100" rows="20">
 
 
   			 </textarea>
-
-
-
-
-
   		</table>
-  		 <input type="hidden" name="pid" value="<?=$_GET['class_id'];?>">;
+  		 <input type="hidden" name="pid" value="<?=$_GET['classid'];?>">;
         <input type="submit" value="发表">
 
   </form>
