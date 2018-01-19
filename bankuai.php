0e1ef9ba5bad3c207492176b731e88273371ffb0
etrnal1 <etrnal1@users.noreply.github.com>
2018-01-19 11:11:19 +0800

    实现简单的添加板块的内容

---
+++ bankuai.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>添加板块</title>
+</head>
+<body>
+	 <form action="rbankuai.php" method="post">
+
+       板块  <input type="text" name="category">
+
+
+		<input type="submit" value="添加板块">
+	 </form>
+
+</body>
+</html>
