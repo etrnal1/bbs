45e9ddf607853c3ec063b2c4374772f3902c8fab
etrnal1 <etrnal1@users.noreply.github.com>
2018-01-19 15:39:40 +0800

    进行了错误页面的重写

--- tpl/error.php
+++ tpl/error.php
@@ -1,28 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>这是错误信息</title>
-	<style type="test/css">
+	<title>错误</title>
+	<style type="text/css">
 		.error{
-
-			width: 300px;
-			height: 300px;
-			background: orange;
-			color: red;
-			border: 1px solid #ccc;
+			width:300px;
+			height:100px;
+			background:orange;
+			color:#fff;
+			border:1px solid #ccc;
 			position: absolute;
 			top:40%;
-			left: 40%;
+			left:40%;
+			font-size:18px;
 			line-height: 100px;
 			text-align: center;
 		}
 	</style>
 </head>
 <body>
-	<div class ="error">
-		显示错误
-
+	<div class="error">
+		<?=$tips;?>
 	</div>
 
 </body>
