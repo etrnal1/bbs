6d0c4c7ee31cd5c2e90ce0c246047ee1dd81cdaf
etrnal1 <etrnal1@users.noreply.github.com>
2018-01-19 08:48:33 +0800

    展示 了更新代码
    用户更新页面，实现了更代码

--- user_update.php
+++ user_update.php
@@ -6,18 +6,29 @@ include 'common.php';
 //var_dump($_POST);
 
 $id = $_POST['id'];
-$username = $_POST['username'];
-//准备sql语句
-$sql = "update bs_user set username = '$username' where uid ='$id'";
+$username = trim($_POST['username']);
+
+//准备sql语句 判断是否有密码。如果有密码可以对密码进行更新
+
+if (!empty($_POST['password'])) {
+	$password = md5(trim($_POST['rpwd']));
+	$sql = "update bs_user set username = '$username'  ,password ='$password' where uid ='$id'";
+	echo $sql;
+
+} else {
+
+	echo '无权限修改密码';
+
+}
 $result = mysqli_query($conn, $sql);
 //var_dump($result);
 if ($result) {
 	echo '更新成功';
-	echo '<a href="index.html">点击进首页</a>';
+	echo '<a  href="index.html">点击进首页</a>';
 
 } else {
 	echo '更新失败';
-	echo '<a href="hah ">点击返回修改页面</a>';
+	echo '<a href="' . $_SERVER['HTTP_REFERER'] . '">点击返回修改页面</a>';
 }
 mysqli_close($conn);
 
