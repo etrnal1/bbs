7f9640af96c83f3a458773599227ff12df5ea071
etrnal1 <etrnal1@users.noreply.github.com>
2018-01-20 21:10:57 +0800

    进行了页面的删除
    试试

---
+++ cache/list_html.php
@@ -0,0 +1,417 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"/>
+<title>新帖</title>
+
+<link href="css/section.css" type="text/css" rel="stylesheet" /> 
+<link href="css/footer.css" type="text/css" rel="stylesheet" /> 
+
+<style>
+body{
+background:#F1F5F8;
+}
+	section{
+		
+		width:100%;
+		height:1780px;
+	}
+	section .right{
+	margin-top:60px;
+	width:281px;
+	height:1660px;
+	background:white;
+	float:left;
+	margin-left:10px;
+	}
+	section .left{
+	padding-top:0px;
+	margin-top:60px;
+	width:700px;
+	height:1660px;
+	background:#fff;
+	margin-left:-100px;
+	float:left;
+	}
+	section .center{
+	 margin-top:60px;
+	width:190px;
+	height:1660px;
+	background:white;
+	margin-left:10px;
+	float:left;
+	}
+	}
+	.top-di{
+		width:500px;
+		height:30px;
+		
+	}
+	.top-di img{
+		margin-left:90px;
+		line-height:48px;
+		margin-top:13px;
+		float:left;
+	}
+	.top-di p{
+		margin-left:10px;
+		height:30px;
+		line-height:30px;
+		float:left;
+		font-size:12px;
+		color:#666666;
+		margin-left:5px;
+	}
+	.top-di a{
+		text-decoration:none;
+		color:#666666;
+	}
+	.top-di a:hover{
+		text-decoration:underline;
+		color:#666666;
+	}
+	
+ section .left table{
+	margin:0px auto;
+	width:662px;
+	background:#fff;
+	height:1000px;
+ }
+ section .left table .img{
+	float:left;
+	width:50px;
+	height:50px;
+ }
+  section .left table tr td{
+  border-bottom:1px #000 dashed;
+  margin-top:10px;
+	
+ }
+ section .left table .div{
+	float:left;
+	margin-left:11px;
+ }
+  section .left table .div .a1:link{
+	color:#105CB6;
+	font-size:14px;
+	text-decoration:none
+
+ }
+ section .left table .div .a1:hover{
+	text-decoration:underline;
+ }
+ section .left table .div .a2:link{
+	color:#00B050;
+	font-weight:bold;
+	font-size:14px;
+	text-decoration:none;
+ }
+ section .left table .div .p1{
+	font-size:12px;
+	color:#999999;
+ }
+ section .left table .div .p1 .neibu:link{
+	font-size:12px;
+	color:#999999;
+	text-decoration:none;
+
+ }
+  section .left table .div .p1 .neibu:hover{
+	
+	text-decoration:underline;
+
+ }
+ section .left table .div .a2:hover{
+	text-decoration:underline;
+	
+ }
+ section .left table .p{
+	margin-left:11px;
+ }
+ section .left table .p2{
+	float:left;
+	background:#F4F4F4;
+	font-size:14px;
+	color:#999999;
+	margin-left:3px;
+	text-align:center;
+	border-radius:3px;
+
+ }
+  section .left table .p3{
+	float:right;
+	background:#F4F4F4;
+	font-size:14px;
+	color:#999999;
+	padding-left:10px;
+	padding-right:10px;
+	border-radius:3px;
+
+
+ }
+ section .left .top{
+	width:662px;
+	height:46px;
+	background:#fff;
+	border-bottom:1px solid #E3E3E3;
+	margin:0px auto;
+ }
+section .left .top .top-left{
+	float:left;
+	font-size:14px;
+	margin-top:20px;
+ }
+ section .left .top .top-left a:link{
+	color:#fff;
+	background:#4791D0;
+	border-radius:2px;
+	text-decoration:none;
+	font-weight:bold;
+	padding-left:8px;
+	padding-right:8px;
+	padding-top:4px;
+	padding-bottom:4px;
+ }
+ section .left .top .top-right{
+	float:right;
+
+	font-size:14px;
+	margin-top:20px;
+	
+ }
+
+section .left .top .top-right a:link{
+	color:#666666;
+	text-decoration:none;
+	border:1px solid #CCCCCC;
+	padding:4px;
+	border-radius:2px;
+
+ }
+section .left .left-di{
+	width:662px;
+	height:58px;
+	margin:0px auto;
+}
+section .left .left-di .number{
+	width:300px;
+	height:36px;
+	padding-top:12px;
+}
+ 
+
+ button{
+ width:30px;
+ height:30px;
+ }
+  button:hover{
+ width:30px;
+ height:30px;
+ color:#fff;
+ background:#0078D8;
+ border:none;
+ border-radius:3px;
+ cursor:pointer;
+ }
+ section .centera{
+	width:179px;
+	height:37px;
+	background:white;
+	margin:0px auto;
+	line-height:37px;
+	font-size:14px;
+	font-weight:bold;
+	border-bottom:1px solid #E8E8E8;
+ }
+ section .centerb{
+	width:179px;
+	height:160px;
+	background:white;
+	margin:0px auto;
+	line-height:37px;
+	font-size:14px;
+	border-bottom:1px dashed #E8E8E8;
+ }
+ section .center .centerb .one{
+	text-decoration:none;
+	height:30px;
+	width:178px;
+	display:block;
+	background:white;
+	line-height:30px;
+	font-size:12px;
+	color:#333333;
+	padding-top:10px;
+}
+section .center .centerb .one .ee{
+margin:0px;
+padding:0px;
+
+}
+
+section .center .centerb .one:link{
+	text-decoration:none;
+	font-size:12px;
+	height:30px;
+	line-height:30px;
+	color:#333333;
+	
+}
+
+section .center .centerb img{
+margin-left:45px;
+}
+ section .center .centerb .one:hover{
+	width:179px;
+	height:30px;
+	background:#eeeeee;
+	text-decoration:underline;
+	line-height:30px;
+	padding-top:3px;
+	
+ }
+ 
+ 
+ section .right input{
+	 font-weight:bold;
+	 width:110px;
+	 margin-left:10px;
+	 margin-top:50px;
+	 margin-right:20px;
+	 height:30px;
+	 float:left;
+ }
+ .right input[name=re_btn]{
+	 background-color:orange;
+	 border:1px solid gray;
+	 color:white;
+ }
+ .right input[name=lo_btn]{
+	 background-color:blue;
+	 border:1px solid gray;
+	 color:white;
+ }
+
+</style>
+</head>
+<body>
+<div class="top-di">
+		<img src="<?=$webSite;?>img/image/xbt.png" />
+		<p>
+			<a href="index.php">首页
+			</a>
+				>
+			<a href="#">本站新帖
+			</a>
+		</p>
+	</div>
+<section>
+	<div class="left">
+		<div class="top">
+			<div class="top-right"><a href="#">最新发帖</a><a href="#" style="background:#F2F2F2;color:#999999;">最后回复</a>
+			</div>
+			<div class="top-left">
+				<a href="#">本站新帖</a>
+			</div>
+			
+		</div>
+		<table>
+<!-- 最开始一个 -->
+			<tr>
+				<td>
+					<img class="img" src="<?=$webSite;?>img/image/x5.png"/>
+					<div class="div">
+						<img src="<?=$webSite;?>img/image/xdj2.png"/>
+						<a href="#" class="a1">[程序发布]</a>
+						<a href="#" class="a2"> PW移动社区9.0正式推出，开放公测（2015.11...</a>
+						<img src="<?=$webSite;?>img/image/youjian.png" />
+						
+						<p class="p1">楼主：
+							<a href="#" class="neibu">PW相即</a>
+							2015-03-11 最后回复：
+							<a href="#" class="neibu">喂丶胡儿</a>
+							07-24 13:40</p>
+					</div>
+				</td>
+				<td>
+					<div class="p2">
+						浏览
+						<p>352123</p>
+					</div>
+					<div class="p3">
+						回复
+						<p>388</p>
+					</div>
+				</td>
+			</tr>
+		<!-- 最后一个 -->
+		</table>
+		<div class="left-di">
+			<div class="number">
+				<button>1</button>
+				<button>2</button>
+				<button>3</button>
+				<button>4</button>
+				<button style="width:51px;">…24</button>
+				<button style="width:78px;">下一页>></button>
+			</div>
+			
+		</div>
+	</div>
+	<div class="center">
+		<div class="centera">板块列表</div>
+		<div class="centerb">
+			<a href="#" class="one">phpwind 官方动态
+			<img src="image/xl.png" />
+			</a>
+			<a href="#" class="one"><img class="ee"src="image/list.png" />phpwind 官方动态
+			
+			</a>
+			<a href="#" class="one"><img class="ee" src="image/list.png" />phpwind 官方动态
+			
+			</a>
+			<a href="#" class="one"><img class="ee" src="image/list.png" />phpwind 官方动态
+			
+			</a>
+		</div>
+		<div class="centerb">
+			<a href="#" class="one">phpwind 官方动态
+			<img src="image/xl.png" />
+			</a>
+			<a href="#" class="one"><img class="ee"src="image/list.png" />phpwind 官方动态
+			
+			</a>
+			<a href="#" class="one"><img class="ee" src="image/list.png" />phpwind 官方动态
+			
+			</a>
+			<a href="#" class="one"><img class="ee" src="image/list.png" />phpwind 官方动态
+			
+			</a>
+		</div>
+		<div class="centerb">
+			<a href="#" class="one">phpwind 官方动态
+			<img src="image/xl.png" />
+			</a>
+			<a href="#" class="one"><img class="ee"src="image/list.png" />phpwind 官方动态
+			
+			</a>
+			<a href="#" class="one"><img class="ee" src="image/list.png" />phpwind 官方动态
+			
+			</a>
+			<a href="#" class="one"><img class="ee" src="image/list.png" />phpwind 官方动态
+			
+			</a>
+		</div>
+	</div>
+	<div class="right">
+		<form action="login.php" method="post">
+			<input type="submit" name="lo_btn" value="登录" />
+		</form>
+		<form action="login.php" method="post">
+			<input type="submit" name="re_btn" value="立即注册" />
+		</form>
+	</div>
+	</section>
+
+</body>
+</html>
