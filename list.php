ad062e6a8ada1d4998c2f4877821ac0bf7bc916f
etrnal1 <etrnal1@users.noreply.github.com>
2018-01-20 21:11:39 +0800

    对一些文件
    对一些文件进行了删除，添加了一些代码

---
+++ list.php
@@ -0,0 +1,5 @@
+<?php
+include 'common/common.php';
+
+display('list.html', compact('webSite'));
+?>
---
+++ setting.php
@@ -0,0 +1,5 @@
+<?php
+include 'common/common.php';
+display('setting.html', compact('webSite'));
+
+?>
