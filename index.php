26cae6b6cf5e5925d504600a1a8e3688bf205ab5
etrnal1 <etrnal1@users.noreply.github.com>
2018-01-21 14:15:47 +0800

    进行了修改
    进行修改

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 include 'common/common.php';
-
+//板块表叫bs_category 板块名字叫category
+// $data['pid'] = empty($_POST['pid']) ? 0 : (int) ($_POST['pid']);
+// //得到板块名字
+// $data['category'] = trim($_POST['form_name']);
+// $data['createtime'];
+//查询信息
+// $data = select($conn, DB_PREFIX . 'category', 'id,category', 'id>0');
+// var_dump($data);
 // $data = select($conn, DB_PREFIX . 'user', 'uid,username,password,lasttime,type', "username='$username'");
 // select $conn  $table $files $where;
 // $conn = select($conn,DB_PREFIX.'user','uid,username,password',"uid=$_SESSION['uid']");
