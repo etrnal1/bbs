87a13fbbe5189e77ed3ed1f4bfb7e07068b4d6a8
etrnal1 <etrnal1@users.noreply.github.com>
2018-01-21 14:15:34 +0800

    首页
    首页进行展示

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,11 @@
 <?php
 include '../common/common.php';
+
+//查询大板块
+$data = select($conn, DB_PREFIX . 'category', 'id,category', 'pid=0');
+// $pids = $data;
+//dump($data);
+
 // $data = select($conn,DB_PREFIX.'fourm','fid,fourm_name','pid=0');
 
-display('admin/index.html', compact('webSite'));
+display('admin/index.html', compact('webSite', 'data'));
